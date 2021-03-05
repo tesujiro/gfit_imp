d750b8a7dd17ec2acc5bf207f66def999bb15f00
tesujiro <tesujiro@gmail.com>
2021-03-05 20:38:29 +0900

    add: parse blood pressure

--- bpNoteCSV2json/main.go
+++ bpNoteCSV2json/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -19,38 +20,111 @@ const (
 const (
 	colMorningBP = 2
 	colNightBP   = 5
+	colWeight    = 27
 )
 
 const (
-	BloodPresure BpNoteType = iota
+	BloodPressure BpNoteType = iota
 	Weight
 )
 
 type BpNote struct {
-	bpNoteTyoe BpNoteType
+	bpNoteType BpNoteType
 	timeNanos  int64
 	value      float32
+	highValue  float32
+	lowValue   float32
 }
 
-func time2unix(yyyymmdd, hhmm string) (int64, error) {
+func time2unix(yyyymmdd, hhmm, defaultHhmm string) (int64, error) {
 	if hhmm == "" {
-		hhmm = defaultMorningTime //TODO:
+		hhmm = defaultHhmm
 	}
-	t, err := time.Parse("2006/01/0215:04", yyyymmdd+hhmm)
+	t, err := time.Parse("2006/1/2 15:04", yyyymmdd+" "+hhmm)
 	if err != nil {
 		return 0, err
 	}
 	return t.UnixNano(), nil
 }
 
-func readCsv(filepath string) ([]BpNote, error) {
+func parseBloodPressure(date, hhmm, highStr, lowStr, defaultHhmm string) (BpNote, error) {
+	unixtime, err := time2unix(date, hhmm, defaultHhmm)
+	if err != nil {
+		return BpNote{}, fmt.Errorf("time format error: %v\n", err)
+	}
+	//fmt.Printf("MorningBP(%v,%v,%v)\n", unixtime, line[colMorningBP], line[colMorningBP+1])
+
+	high, err := strconv.ParseFloat(highStr, 32)
+	if err != nil {
+		return BpNote{}, fmt.Errorf("parseFloat error: %s", highStr)
+	}
+	low, err := strconv.ParseFloat(lowStr, 32)
+	if err != nil {
+		return BpNote{}, fmt.Errorf("parseFloat error: %s", lowStr)
+	}
+
+	bp := BpNote{
+		bpNoteType: BloodPressure,
+		timeNanos:  unixtime,
+		highValue:  float32(high),
+		lowValue:   float32(low),
+	}
+	return bp, nil
+}
+
+func parseLine(line []string) ([]BpNote, error) {
+	bpn := []BpNote{}
+	date := line[0]
+	if line[colMorningBP] != "" {
+		col := colMorningBP
+		hhmm := line[col-1]
+		highStr := line[col]
+		lowStr := line[col+1]
+
+		bp, err := parseBloodPressure(date, hhmm, highStr, lowStr, defaultMorningTime)
+		if err != nil {
+			return bpn, err
+		}
+		bpn = append(bpn, bp)
+	}
+	if line[colNightBP] != "" {
+		col := colNightBP
+		hhmm := line[col-1]
+		highStr := line[col]
+		lowStr := line[col+1]
+
+		bp, err := parseBloodPressure(date, hhmm, highStr, lowStr, defaultNightTime)
+		if err != nil {
+			return bpn, err
+		}
+		bpn = append(bpn, bp)
+	}
+	if line[colWeight] != "" {
+		unixtime, err := time2unix(date, "", defaultMorningTime)
+		if err != nil {
+			return bpn, fmt.Errorf("time format error: %v\n", err)
+		}
+		weight, err := strconv.ParseFloat(line[colWeight], 32)
+		if err != nil {
+			return bpn, fmt.Errorf("parseFloat error: %s", line[colWeight])
+		}
+		bp := BpNote{
+			bpNoteType: Weight,
+			timeNanos:  unixtime,
+			value:      float32(weight),
+		}
+		bpn = append(bpn, bp)
+	}
+	return bpn, nil
+}
+func csv2json(filepath string) error {
 	if filepath == "" {
-		return nil, nil
+		return nil
 	}
 
 	file, err := os.Open(filepath)
 	if err != nil {
-		return nil, errors.New("Cannot open file: " + filepath)
+		return errors.New("Cannot open file: " + filepath)
 	}
 	defer file.Close()
 
@@ -63,48 +137,35 @@ func readCsv(filepath string) ([]BpNote, error) {
 		if err == io.EOF {
 			break
 		}
-		fmt.Println(line)
+		if err != nil {
+			return fmt.Errorf("csv read error: %v\n", err)
+		}
+		//fmt.Println(line)
 
-		if lineNumber == 0 {
+		/*
+			for i, col := range line {
+				fmt.Printf("line[%d]\t:%v\n", i, col)
+			}
+		*/
+
+		lineNumber++
+		if lineNumber == 1 {
 			continue
 		}
-		if err != nil {
-			return nil, fmt.Errorf("csv read error: %v\n", err)
-		}
+
 		//if len(line) != 3 {
 		//return nil, fmt.Errorf("csv format error: %v\n", line)
 		//}
 
-		date := line[0]
-		switch {
-		case line[colMorningBP] != "":
-			fmt.Printf("MorningBP(%s,%s,%s)\n", line[colMorningBP-1], line[colMorningBP], line[colMorningBP+1])
-			col := colMorningBP
-			hhmm := line[col-1]
-			unixtime, err := time2unix(date, hhmm)
-			if err != nil {
-				fmt.Printf("MorningBP(%s,%s,%s)\n", line[colMorningBP-1], line[colMorningBP], line[colMorningBP+1])
-				return nil, fmt.Errorf("time format error: %v\n", err)
-			}
-			fmt.Printf("MorningBP(%s,%s,%s)\n", unixtime, line[colMorningBP], line[colMorningBP+1])
+		bpns, err := parseLine(line)
+		if err != nil {
+			return err
 		}
-		/*
-			host := line[0]
-			port_str := line[1]
-			comment := line[2]
-			port, err := strconv.Atoi(port_str)
-			if err != nil {
-				fmt.Println("port number error: " + port_str)
-				return nil, fmt.Errorf("csv format error: %v\n", line)
-			}
-			serverList = append(serverList, server{
-				host:    host,
-				port:    port,
-				comment: comment,
-			})
-		*/
+		for _, bpn := range bpns {
+			fmt.Println(bpn)
+		}
 	}
-	return nil, nil
+	return nil
 }
 
 func main_() int {
@@ -113,7 +174,7 @@ func main_() int {
 		fmt.Printf("error len(os.Args):%v", len(os.Args))
 		return 1
 	}
-	_, err := readCsv(os.Args[1])
+	err := csv2json(os.Args[1])
 	if err != nil {
 		fmt.Printf("err : %s\n", err)
 	}
